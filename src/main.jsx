92fb410098007d7f55a66a5f00482822e7f328c3
acoji <acoji.gm@gmail.com>
2025-10-02 12:23:51 +0300

    refactor: change routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routing/routes';
+import { router } from './routing/routes.jsx';
 
 import './assets/styles/main.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
---
+++ src/routing/routes.jsx
@@ -0,0 +1,22 @@
+import App from '../App.jsx';
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage.jsx';
+import QuizzesPage from '../pages/QuizesPage/QuizesPage.jsx';
+import QuizPage from '../pages/QuizPage/QuizPage.jsx';
+import ResultPage from '../pages/ResultPage/ResultPage.jsx';
+import NotFound from '../pages/NotFound/NotFound.jsx';
+import { staticLinks } from '../config/staticLinks';
+import { dynamicLinks } from '../config/dynamicLinks';
+
+export const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { path: staticLinks.main, element: <HomePage /> },
+      { path: staticLinks.quizzes, element: <QuizzesPage /> },
+      { path: dynamicLinks.quiz, element: <QuizPage /> },
+      { path: dynamicLinks.result, element: <ResultPage /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
