0ee352b10f8abd66ea488f83671c5e4e96bfb53a
acoji <acoji.gm@gmail.com>
2025-10-16 00:09:07 +0300

    feat: axios configuration

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    port: 3000,
-    open: false,
-  },
   build: {
     sourcemap: true,
     outDir: 'dist',
