38407f87008670c67ca7a7f7dd88aa539362f0f4
acoji <acoji.gm@gmail.com>
2025-10-22 09:55:25 +0300

    feat: add create-question-form

--- src/pages/CreateQuestionPage/CreateQuestionPage.jsx
+++ src/pages/CreateQuestionPage/CreateQuestionPage.jsx
@@ -1,6 +1,7 @@
 import { Header } from '@/components/commons/Header/Header.jsx';
 
 import styles from './CreateQuestionPage.module.scss';
+import CreateQuestionForm from '@/pages/CreateQuestionPage/CreateQuestionForm/CreateQuestionForm.jsx';
 
 const CreateQuestionPage = () => {
   return (
@@ -11,6 +12,7 @@ const CreateQuestionPage = () => {
         <section className={styles.card}>
           <h1 className={styles.title}>Меню создания вопросов</h1>
           <p className={styles.subtitle}>Создайте вопросы для своего квиз</p>
+          <CreateQuestionForm />
         </section>
       </main>
     </div>
--- src/store/form.js
+++ src/store/form.js
@@ -11,6 +11,18 @@ export class FormStore {
     creatorId: null,
   };
 
+  _questionFormData = {
+    text: '',
+    answers: [
+      { text: '', isCorrect: false },
+      { text: '', isCorrect: false },
+      { text: '', isCorrect: false },
+      { text: '', isCorrect: false },
+    ],
+  };
+
+  _questions = [];
+
   constructor() {
     makeAutoObservable(this);
   }
@@ -36,6 +48,53 @@ export class FormStore {
       difficulty: 'EASY',
       category: 'PROGRAMMING',
       creatorId: null,
+    };
+  }
+
+  get questionFormData() {
+    return this._questionFormData;
+  }
+
+  get questions() {
+    return this._questions;
+  }
+
+  setQuestionField(name, value) {
+    this._questionFormData[name] = value;
+  }
+
+  setAnswer(index, text) {
+    this._questionFormData.answers[index].text = text;
+  }
+
+  markAsCorrect(index) {
+    this._questionFormData.answers.forEach((a, i) => {
+      a.isCorrect = i === index;
+    });
+  }
+
+  addQuestion(quizId) {
+    this._questions.push({
+      quizId,
+      text: this._questionFormData.text.trim(),
+      answers: this._questionFormData.answers.map(({ text, isCorrect }) => ({
+        text: text.trim(),
+        isCorrect,
+      })),
+    });
+
+    this.questionFormReset();
+  }
+
+  questionFormReset() {
+    this._questionFormData = {
+      text: '',
+      answers: [
+        { text: '', isCorrect: false },
+        { text: '', isCorrect: false },
+        { text: '', isCorrect: false },
+        { text: '', isCorrect: false },
+      ],
     };
   }
 }
